9873d36a7f5d618c840e3bac5c91145aef7c9d7c
Sanket Harvande <sanketharvande49@gmail.com>
2022-06-09 18:13:50 +0530

    modified changes

---
+++ PepCoding_DSA/Functions_and_Arrays/ArraySwap.java
@@ -0,0 +1,26 @@
+package PepCoding_DSA.Functions_and_Arrays;
+
+public class ArraySwap {
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr;
+        arr = new int[5];
+        arr[0] = 11;
+        arr[1] = 22;
+        arr[2] = 65;
+        arr[3] = 69;
+        arr[4] = 98;
+
+        swap(arr, 0, 4);
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
