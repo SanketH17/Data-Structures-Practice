f4859e4eb6e89a402017d14b3ad5492fd7b50f7d
Sanket Harvande <sanketharvande49@gmail.com>
2022-06-12 11:17:16 +0530

    done

---
+++ PepCoding_DSA/2_2D_Arrays/P1_Intro.java
@@ -0,0 +1,29 @@
+package PepCoding_DSA.2_2D_Arrays;
+
+import java.util.*;
+
+public class P1_Intro {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int [][] arr = new int[n][m];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        //Printing the elements
+        // arr.length ---->> gives length of ROWS
+        // arr[0].length ------>> gives length of COLUMNS
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                System.out.print(arr[i][j]);
+            }
+        }
+
+        sc.close();
+    }
+}
