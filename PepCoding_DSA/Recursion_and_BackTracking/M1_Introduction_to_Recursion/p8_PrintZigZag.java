bc79f614fc9cb18d21fade314765a6f5600a8425
Sanket Harvande <sanketharvande49@gmail.com>
2022-06-17 06:14:19 +0530

    Print Zig zag and flow of recursion

---
+++ PepCoding_DSA/Recursion_and_BackTracking/M1_Introduction_to_Recursion/p8_PrintZigZag.java
@@ -0,0 +1,20 @@
+package PepCoding_DSA.Recursion_and_BackTracking.M1_Introduction_to_Recursion;
+
+public class p8_PrintZigZag {
+    public static void main(String[] args) {
+        pzz(3);
+    }
+
+    public static void pzz(int n){
+        if(n == 0){
+            return;
+        }
+
+        System.out.println("Pre" + n);
+        pzz(n - 1);
+        System.out.println("In" + n);
+        pzz(n - 1);
+        System.out.println("Post" + n);
+
+    }
+}
