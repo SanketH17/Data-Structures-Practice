1c22df444f300839e5fff37f39f549814828da10
Sanket Harvande <sanketharvande49@gmail.com>
2022-06-18 12:27:30 +0530

    Get subsequences in array list using recursion

---
+++ PepCoding_DSA/Recursion_and_BackTracking/M3_Recursion_in_ArrayList/hello.java
@@ -0,0 +1,5 @@
+package PepCoding_DSA.Recursion_and_BackTracking.M3_Recursion_in_ArrayList;
+
+public class hello {
+    
+}
---
+++ PepCoding_DSA/Recursion_and_BackTracking/M3_Recursion_in_ArrayList/p1_getSubsequence.java
@@ -0,0 +1,43 @@
+package PepCoding_DSA.Recursion_and_BackTracking.M3_Recursion_in_ArrayList;
+
+
+import java.util.*;
+
+public class p1_getSubsequence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        ArrayList<String> res;
+        res = gss(str);
+        System.out.println(res);
+
+        sc.close();
+    }
+
+
+    public static ArrayList<String> gss(String str){
+
+        // eg. str = abc
+
+        if(str.length() == 0){
+            ArrayList<String> bres = new ArrayList<>();
+            bres.add("");
+            return bres;
+        }
+        char ch = str.charAt(0); // a
+        String ros = str.substring(1); // bc, ros -> rest of the string
+        ArrayList<String> rres = gss(ros); // rres -> recursion result
+
+
+        ArrayList<String> mres = new ArrayList<>(); // mres -> my result
+        for(String rstr : rres){
+            mres.add("" + rstr);
+        }
+        for(String rstr : rres){
+            mres.add(ch + rstr);
+        }
+
+        return mres;
+
+    }
+}
