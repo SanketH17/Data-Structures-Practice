eaece66add3ed2937eb063b9fc0b732482eb4aee
Sanket Harvande <sanketharvande49@gmail.com>
2022-06-18 13:25:35 +0530

    Get keypad combination

--- PepCoding_DSA/Recursion_and_BackTracking/M3_Recursion_in_ArrayList/p2_GetKeypadCombination.java
+++ PepCoding_DSA/Recursion_and_BackTracking/M3_Recursion_in_ArrayList/p2_GetKeypadCombination.java
@@ -3,9 +3,9 @@ package PepCoding_DSA.Recursion_and_BackTracking.M3_Recursion_in_ArrayList;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import javax.sql.rowset.spi.SyncResolver;
 
-import java.util.*;
+
+
 
 public class p2_GetKeypadCombination {
     public static void main(String[] args) {
@@ -15,6 +15,37 @@ public class p2_GetKeypadCombination {
 
     System.out.println(words);
 
+    sc.close();
+
     }
-    
+
+    static String[] codes = {".;", "abc", "def", "ghi", "jkl", "mno","pqrs", "tu", "vwx", "yz" };
+
+    public static ArrayList<String> getKPC(String str) {
+
+
+        if(str.length() == 0){
+            ArrayList<String> bres = new ArrayList<>();
+            bres.add("");
+            return bres;
+        }
+
+        char ch = str.charAt(0);
+        String ros = str.substring(1);
+        ArrayList<String> rres = getKPC(ros);
+
+        ArrayList<String> mres = new ArrayList<>();
+
+        String codeforch = codes[ch - '0'];
+
+        for(int i = 0; i < codeforch.length(); i++){
+            char chcode = codeforch.charAt(i);
+            for(String rstr : rres){
+                mres.add(chcode + rstr);
+            }
+        }
+
+        return mres;
+
+    }
 }
