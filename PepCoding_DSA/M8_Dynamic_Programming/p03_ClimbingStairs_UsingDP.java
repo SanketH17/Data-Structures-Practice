f8a4b79d897d0ef046a01165b180690bcec04533
Sanket Harvande <sanketharvande49@gmail.com>
2022-06-25 11:08:08 +0530

    Climbing stairs using tabulation- dp

--- PepCoding_DSA/M8_Dynamic_Programming/p03_ClimbingStairs_UsingDP.java
+++ PepCoding_DSA/M8_Dynamic_Programming/p03_ClimbingStairs_UsingDP.java
@@ -4,31 +4,26 @@ public class p03_ClimbingStairs_UsingDP {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int cp = countPaths(n, new int[n + 1]);
+        int cp = countPathsTab(n);
         System.out.println(cp);
         sc.close();
     }
 
-    public static int countPaths(int n, int[] qb) {
-        if(n == 0){
-            return 1;
-        }
-        else if(n < 0){
-            return 0;
-        }
-        if(qb[n] > 0){
-            return qb[n];
-        }
+    public static int countPathsTab(int n) {
+        int[] dp = new int[n + 1];
 
-        System.out.println("Hello " + n);
-        int nm1 = countPaths(n - 1, qb);
-        int nm2 = countPaths(n - 2, qb);
-        int nm3 = countPaths(n - 3, qb);
-        int cp = nm1 + nm2 + nm3;
-
-        qb[n] = cp;
-
-        return cp;
+        dp[0] = 1;
+        for(int i = 1; i <= n; i++){
+            if(i == 1){
+                dp[i] = dp[i - 1];
+            }else if(i == 2){
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            else{
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i-3];
+            }
+        }
+        return dp[n];
     }
 }
 
