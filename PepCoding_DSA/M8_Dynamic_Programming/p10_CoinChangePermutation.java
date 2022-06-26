5250ed672d28455633457954edff72a12ed33cfe
Sanket Harvande <sanketharvande49@gmail.com>
2022-06-27 02:41:30 +0530

    coin change permutation

--- PepCoding_DSA/M8_Dynamic_Programming/p10_CoinChangePermutation.java
+++ PepCoding_DSA/M8_Dynamic_Programming/p10_CoinChangePermutation.java
@@ -1,10 +1,10 @@
-
-
 /*
-
+Sample input :
 4
 2 3 5 6
 10
+
+Sample output :
 17
 
  */
@@ -38,3 +38,19 @@ public class p10_CoinChangePermutation {
         sc.close();
     }
 }
+
+/*
+
+Time Complexity :
+O(N * Amount)
+
+Since we are running two nested loops, outer loop from 1 to amount, and the inner loop from 0 to n-1 (through coins denominations), 
+hence the time complexity will be O(n * amount).
+
+SPACE COMPLEXITY :
+O(Amount)
+
+We used a 1D dp array of size = amount + 1 to count all the permutations possible. Hence, auxiliary space of O(amount) is used.
+
+
+ */
