a38b0c6b44b056046a1c0f89c99240000b7520f9
Sanket Harvande <sanketharvande49@gmail.com>
2022-06-30 17:06:24 +0530

    done

---
+++ PepCoding_DSA/M8_Dynamic_Programming/p26_HighWay_Billboard.java
@@ -0,0 +1,68 @@
+
+
+/*
+
+20
+5
+6 7 12 14 15
+5 8 5 3 1
+5
+*/
+
+import java.util.*;
+
+public class p26_HighWay_Billboard {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // the length of the road (in km)
+        int m = sc.nextInt(); // no of boards can be put on the road
+        int[] x = new int[m]; // boards mile no - tell the no of miles at which we can put a board
+        for(int i = 0; i < m; i++){
+            x[i] = sc.nextInt();
+        }
+
+        int[] revenue = new int[m];
+        for(int i = 0; i < m; i++){ // tell the revenue for each board
+            revenue[i] = sc.nextInt(); // the least distance between the two boards 
+        }
+
+        int t = sc.nextInt();
+
+        int ans = solution(m, x, revenue, t);
+        System.out.println(ans);
+        sc.close();
+
+    }
+
+    public static int solution(int m, int[] x, int[] rev, int t){
+
+        int[] dp = new int[x.length];
+        int ans = 0;
+
+        dp[0] = rev[0];
+        for(int i = 1; i < x.length; i++){
+            int max = 0;
+
+            for(int j = 0; j < i; j++){
+                int dist = x[i] - x[j];
+                if(dist > t){
+                    max = Math.max(max, dp[j]);
+                }
+            }
+            dp[i] = max + rev[i];
+            ans = Math.max(ans, dp[i]);
+        }
+
+        return ans;
+    }
+
+}
+
+/*
+20
+5 
+6 7 12 14 15 // board mile no
+5 8 5 3 1 // rev
+5
+*/
