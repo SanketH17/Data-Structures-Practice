62d75c1100d39d47034f65c5984d49bd0c81d8a8
Sanket Harvande <sanketharvande49@gmail.com>
2022-07-01 19:01:58 +0530

    added new files

---
+++ 1. JAVA_OOPS/p01_OOPS.java
@@ -0,0 +1,37 @@
+/*
+The reference variable refers to the object allocated in the heap memory area. Here, s1 and s2 both are reference 
+variables that refer to the objects allocated in memory.
+ */
+
+
+class Student{
+    String name;
+    int age;
+
+    public void getInfo(){
+        System.out.println("The name of this student is " + this.name);
+        System.out.println("The age of this student is " + this.age);
+    }
+
+}
+
+
+
+
+
+
+public class p01_OOPS{
+    public static void main(String[] args) {
+        
+        Student s1 = new Student(); // s1 is an object of class Student
+        s1.age = 24;
+        s1.name = "Sanket";
+        s1.getInfo();
+
+        Student s2 = new Student(); // s2 is another object of a class Student
+        s2.name = "Aditya";
+        s2.age = 20;
+        s2.getInfo(); // method
+
+    }
+}
