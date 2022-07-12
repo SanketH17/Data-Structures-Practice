c7e7b28938600e5c6b5a6f08f91273fc6012fe5f
Sanket Harvande <sanketharvande49@gmail.com>
2022-07-12 18:53:02 +0530

    started with linked list

---
+++ PepCoding_DSA/M10_LinkedList/LinkedList_OOPs/LinkedList.java
@@ -0,0 +1,30 @@
+package M10_LinkedList.LinkedList_OOPs;
+
+/*
+
+this --> by using this hum class ke data members ko access karte hain
+
+
+ */
+public class LinkedList {
+    
+    private class Node {
+        int data;
+        Node next;
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+    
+    public void display() {
+
+        Node temp = this.head ;
+
+        while(temp != null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+
+    }
+}
---
+++ PepCoding_DSA/M10_LinkedList/p01_DataMembers.java
@@ -0,0 +1,24 @@
+package M10_LinkedList;
+
+public class p01_DataMembers {
+    public static class Node {
+        int data ;
+        Node next;
+    }
+
+    public static class LinkedList {
+        Node head;
+        Node tail;
+        int size;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+
+
+
+
