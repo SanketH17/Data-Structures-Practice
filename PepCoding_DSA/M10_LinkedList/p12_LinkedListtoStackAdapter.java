1039e4f49d7a59ee1c7304965c58cc75a138d615
Sanket Harvande <sanketharvande49@gmail.com>
2022-07-14 08:48:34 +0530

    Linked list to stack adaptor

---
+++ PepCoding_DSA/M10_LinkedList/p12_LinkedListtoStackAdapter.java
@@ -0,0 +1,61 @@
+package M10_LinkedList;
+/*
+ctrl + shift + k = delete comment row at once
+
+alt + cursor = multicursors for deleting same line contents
+ */
+import java.io.*;
+import java.util.*;
+
+public class p12_LinkedListtoStackAdapter {
+
+    public static class LLToStackAdapter {
+        LinkedList<Integer> list;
+
+        public LLToStackAdapter() {
+            list = new LinkedList<>();
+        }
+
+        int size() {
+            return list.size();
+        }
+
+        void push(int val) {
+            list.addFirst(val);
+        }
+
+        int pop() {
+             return list.removeFirst();
+        }
+
+        int top() {
+            return list.getFirst();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        LLToStackAdapter st = new LLToStackAdapter();
+
+        String str = br.readLine();
+        while (str.equals("quit") == false) {
+            if (str.startsWith("push")) {
+                int val = Integer.parseInt(str.split(" ")[1]);
+                st.push(val);
+            } else if (str.startsWith("pop")) {
+                int val = st.pop();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("top")) {
+                int val = st.top();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("size")) {
+                System.out.println(st.size());
+            }
+            str = br.readLine();
+        }
+    }
+}
