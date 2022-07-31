5c771415a731f8c8be2538157ffc20e605858f4d
Sanket Harvande <sanketharvande49@gmail.com>
2022-07-31 07:35:35 +0530

    Modified changes

--- PepCoding_DSA/M11_Generic_Trees/p01_DataMembers.java
+++ PepCoding_DSA/M11_Generic_Trees/p01_DataMembers.java
@@ -5,16 +5,13 @@ import java.util.ArrayList;
 
 
 
-public class p02_GTConstructor {
+public class p01_DataMembers {
     
     
     private class Node {
         int data;
         ArrayList<Node> children = new ArrayList<>();
     }
-    
-    
-    
     
     
     public static void main(String[] args) {
---
+++ PepCoding_DSA/M11_Generic_Trees/p02_Constructor.java
@@ -0,0 +1,39 @@
+package M11_Generic_Trees;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class p02_Constructor {
+    
+
+    private class Node {
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        
+        int[] arr = {10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1};
+        Node root;
+        Stack<Node> st = new Stack<>();
+
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == -1) {
+                st.pop();
+            } else {
+                Node t = new Node();
+                t.data = arr[i];
+
+                if(st.size() > 0) {
+                    st.peek().children.add(t); // add node to the aL of peek element
+                } else {
+                    root = t;
+                }
+                st.push(t);
+            }
+        }
+
+    }
+
+
+}
