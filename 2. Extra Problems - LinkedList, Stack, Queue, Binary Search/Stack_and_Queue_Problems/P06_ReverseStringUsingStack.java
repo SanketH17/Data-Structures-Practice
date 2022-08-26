aa9283a3e52b6a345fdfbc65df3bf4b266ed8fdd
Sanket Harvande <sanketharvande49@gmail.com>
2022-08-26 22:25:30 +0530

    reverse string using stack

---
+++ 2. Extra Problems - LinkedList, Stack, Queue, Binary Search/Stack_and_Queue_Problems/P06_ReverseStringUsingStack.java
@@ -0,0 +1,31 @@
+package Stack_and_Queue_Problems;
+
+import java.util.*;
+import java.util.Stack;
+
+public class P06_ReverseStringUsingStack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        Stack<Character> st = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            st.push(ch);
+        }
+
+        String ans = "";
+
+        for(int i = 0; i < s.length(); i++) {
+            char ch = st.pop();
+            ans += ch;
+        }
+
+        System.out.println(ans);
+
+        sc.close();
+
+    }
+}
