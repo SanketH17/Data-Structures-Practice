0e766d4b84e2569db8f13ec0ef49fa1c591062e8
Sanket Harvande <sanketharvande49@gmail.com>
2022-08-28 20:13:43 +0530

    Kevin and his fruits - binary search

---
+++ 2. Extra Problems - LinkedList, Stack, Queue, Binary Search/Binary_Search/P23_KevinAndHisFruits.java
@@ -0,0 +1,60 @@
+package Binary_Search;
+
+import java.util.*;
+
+public class P23_KevinAndHisFruits {
+
+    public static int getMaxMarker(int[] arr, int n, int m) {
+        int MIN = 9999999;
+        int MAX = 0;
+
+        for (int i = 0; i < n; i++) {
+            MIN = Math.min(MIN, arr[i]);
+            MAX = Math.max(MAX, arr[i]);
+        }
+        int left = 0;
+        int right = MAX;
+        int idx = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int count = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (arr[i] > mid) {
+                    count += (arr[i] - mid);
+                }
+            }
+
+            if (count == m) {
+                return mid;
+            }
+
+            else if (count < m) {
+                right = mid - 1;
+            }
+
+            else {
+                idx = mid;
+                left = mid + 1;
+            }
+
+        }
+        return idx;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt(); // max fruits kevin can eat
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(getMaxMarker(arr, n, m));
+        sc.close();
+    }
+}
