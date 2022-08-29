9c508028b48d3555474cfe81150761dd7ab9cc9f
Sanket Harvande <sanketharvande49@gmail.com>
2022-08-29 21:13:46 +0530

    infix to postfix

---
+++ 2. Extra Problems - LinkedList, Stack, Queue, Binary Search/Stack_Problems/P10_InfixToPostfix.java
@@ -0,0 +1,73 @@
+package Stack_Problems;
+
+import java.util.*;
+import java.util.Stack;
+
+public class P10_InfixToPostfix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String exp = sc.next();
+
+        sc.close();
+        
+
+        Stack<String> postfix = new Stack<>();
+        Stack<Character> ops = new Stack<>();
+
+       
+
+
+        for(int i = 0; i < exp.length(); i++) {
+            char ch = exp.charAt(i);
+
+            if(ch == '(') {
+                ops.push(ch);
+            } 
+            
+            else if ((ch >= '0' && ch <= '9') || (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+                postfix.push(ch + "");
+            } 
+            
+            else if (ch == ')') {
+                while(ops.peek() != '(') {
+                    process(ops, postfix);
+                }
+                ops.pop(); // pop openenig operator
+            } 
+            
+            else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+                while(ops.size() > 0 && ops.peek() != '(' && precedene(ch) <= precedene(ops.peek())) {
+                    process(ops, postfix);
+                }
+                ops.push(ch);// push current character
+            }
+        }
+
+        while(ops.size() > 0) {
+            process(ops, postfix);
+        }
+
+        System.out.println(postfix.pop());
+
+    }
+
+    public static void process(Stack<Character> ops, Stack<String> postfix) {
+        char op = ops.pop();
+
+        String postv2 = postfix.pop();
+        String postv1 = postfix.pop();
+        String postv = postv1 + postv2 + op;
+        postfix.push(postv);
+    }
+
+    public static int precedene(char op) {
+        if(op == '+' || op == '-') {
+            return 1;
+        } else if (op == '*' || op == '/') {
+            return 2;
+        } else {
+            return 0;
+        }
+    }
+}
