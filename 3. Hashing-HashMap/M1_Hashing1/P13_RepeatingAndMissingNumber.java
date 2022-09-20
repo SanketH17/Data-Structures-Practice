d7b0f61b71812207e47ce694c37d6e900bacbc3a
Sanket Harvande <sanketharvande49@gmail.com>
2022-09-21 00:05:08 +0530

    Repeating and missing numbers in array

---
+++ 3. Hashing-HashMap/M1_Hashing1/P13_RepeatingAndMissingNumber.java
@@ -0,0 +1,35 @@
+import java.util.*;;
+
+public class P13_RepeatingAndMissingNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int repeating = 0;
+        int missing = 0;
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i = 0; i < n; i++) {
+            if(hs.contains(arr[i])) {
+                repeating = arr[i];
+               // break;
+            } else {
+                hs.add(arr[i]);
+            }
+        }
+
+
+        for(int i = 1; i <= n; i++) {
+            if(!hs.contains(i)) {
+                missing = i;
+            }
+        }
+        System.out.print(repeating + " " + missing);
+
+        sc.close();
+    }
+}
