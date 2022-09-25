33bf4c8e03b033efd68e5219584229d796966945
Sanket Harvande <sanketharvande49@gmail.com>
2022-09-25 17:39:21 +0530

    Rahul and minimum subarray

---
+++ 3. Hashing-HashMap/M1_Hashing1/SlidingWindow/P01_RahulAndMinimumSubarray.java
@@ -0,0 +1,34 @@
+package SlidingWindow;
+
+import java.util.Scanner;
+
+public class P01_RahulAndMinimumSubarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int target = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int ans = findMinLengthSubArray(arr, n, target);
+        System.out.println(ans);
+        sc.close();
+    }
+
+    public static int findMinLengthSubArray(int[] arr, int n, int target) {
+        int si = 0, ei = 0, sum = 0, minLength = Integer.MAX_VALUE;
+        while (ei < n) {
+            sum += arr[ei];
+            ei++;
+            while (sum > target) {
+                minLength = Math.min(minLength, ei - si);
+                sum -= arr[si];
+                si++;
+            }
+        }
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+}
