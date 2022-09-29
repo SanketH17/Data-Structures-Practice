3f22e510d420ef0bc5ca2a1aa794c87b0aab1018
Sanket Harvande <sanketharvande49@gmail.com>
2022-09-29 09:52:13 +0530

    Insert Node in BST

---
+++ 3. Hashing-HashMap/M4_BST/P05_InsertNodeInBST.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+class Node {
+    int val;
+    Node left;
+    Node right;
+
+    public Node(int key) {
+        val = key;
+        left = right = null;
+    }
+}
+
+class BST {
+    Node root;
+
+    public BST() {
+        root = null;
+    }
+
+    void insert(int key) {
+        root = insertRec(root, key);
+    }
+
+    public Node insertRec(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+
+        else if (key < root.val) {
+            root.left = insertRec(root.left, key);
+        } else if (key > root.val) {
+            root.right = insertRec(root.right, key);
+        }
+
+        return root;
+    }
+
+    void preorder() {
+        preorderRec(root);
+    }
+
+    public void preorderRec(Node root) {
+        if (root == null) {
+            return;
+        }
+        
+        System.out.print(root.val + " ");
+        preorderRec(root.left);
+        preorderRec(root.right);
+    }
+
+}
+
+public class P05_InsertNodeInBST {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        BST tree = new BST();
+        for (int i = 0; i < n; i++) {
+            tree.insert(arr[i]);
+        }
+        tree.insert(k);
+        tree.preorder();
+
+        sc.close();
+    }
+}
