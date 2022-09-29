c3efe71c87eb5b609137626c3aace58aa872ed70
Sanket Harvande <sanketharvande49@gmail.com>
2022-09-29 08:35:44 +0530

    MinElement in BST

---
+++ 3. Hashing-HashMap/M4_BST/P04_MinimumElementInBST.java
@@ -0,0 +1,75 @@
+
+
+
+import java.util.*;
+
+class Node {
+    int val;
+    Node left;
+    Node right;
+
+    public Node(int key) {
+        val = key;
+        left = right = null;
+    }
+}
+
+class BST {
+    Node root;
+
+    public BST() {
+        root = null;
+    }
+
+    void insert(int key) {
+        root = insertRec(root, key);
+    }
+
+    public Node insertRec(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+
+        else if (key < root.val) {
+            root.left = insertRec(root.left, key);
+        } else if (key > root.val) {
+            root.right = insertRec(root.right, key);
+        }
+
+        return root;
+    }
+
+    public int MinValueInBST(Node root) {
+        if(root == null) {
+            return -1;
+        }
+        while(root.left != null) {
+            root = root.left;
+        }
+        return root.val;
+    }
+
+
+}
+
+public class P04_MinimumElementInBST {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        BST tree = new BST();
+        for (int i = 0; i < n; i++) {
+            tree.insert(arr[i]);
+        }
+        int min = tree.MinValueInBST(tree.root);
+        System.out.println(min);
+
+        sc.close();
+    }
+}
+
