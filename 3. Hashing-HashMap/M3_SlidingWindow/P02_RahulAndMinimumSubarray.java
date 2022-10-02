b6c0e9bf55225a0329137515ccf279dfc1784188
Sanket Harvande <sanketharvande49@gmail.com>
2022-10-02 07:38:23 +0530

    Rahul And Minimum Subarray

--- 3. Hashing-HashMap/M3_SlidingWindow/P02_RahulAndMinimumSubarray.java
+++ 3. Hashing-HashMap/M3_SlidingWindow/P02_RahulAndMinimumSubarray.java
@@ -1,11 +1,22 @@
 package M3_SlidingWindow;
 
+import java.util.*;
 
+/*
 
-import java.util.Scanner;
+find the length of the smallest subarray(subarray is a contiguous 
+part of an array/list) in a given array/list ‘ARR’ of size ‘N’ 
+with its sum greater than a given value. 
+If there is no such subarray return 0.
+
+Example: Given an ‘ARR’: [1, 2, 21, 7, 6, 12] and a number ‘X’: 23. 
+The length of the smallest subarray is 2 as the subarray is [21, 7]
+
+*/
 
 public class P02_RahulAndMinimumSubarray {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code here
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
@@ -17,6 +28,7 @@ public class P02_RahulAndMinimumSubarray {
 
         int ans = findMinLengthSubArray(arr, n, target);
         System.out.println(ans);
+
         sc.close();
     }
 
@@ -33,4 +45,5 @@ public class P02_RahulAndMinimumSubarray {
         }
         return minLength == Integer.MAX_VALUE ? 0 : minLength;
     }
+
 }
