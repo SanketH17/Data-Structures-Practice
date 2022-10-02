540b7843ce83c8303cae3b8ef086404df2143499
Sanket Harvande <sanketharvande49@gmail.com>
2022-10-02 07:05:59 +0530

    done

---
+++ 3. Hashing-HashMap/ContestProblems/P03_SmallerThanTripletSum.java
@@ -0,0 +1,43 @@
+package ContestProblems;
+
+
+import java.util.*;
+
+
+public class P03_SmallerThanTripletSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+
+        int ans = findTriplets(arr, n, target);
+        System.out.println(ans);
+
+        sc.close();
+
+    }
+
+    public static int findTriplets(int[] arr, int n, int target) {
+        int count = 0, start = 0, end = 0, sum = 0, ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            sum += arr[i];
+
+            if(sum >= target) {
+                count++;
+            }
+            while(sum >= target) {
+                sum -= arr[start];
+                start++;
+            }
+        }
+        return count;
+    }
+
+}
