d9477c860b4e87acbad978022290dd08edfb2858
Sanket Harvande <sanketharvande49@gmail.com>
2022-10-23 08:44:06 +0530

    deleted

--- 1. Extra Problems - Arrays, Strings, Recursion/M2_Loops_and_Arrays/Arrays_Gym/P22_MinLightsToActivate.java
+++
@@ -1,9 +0,0 @@
-package M2_Loops_and_Arrays.Arrays_Gym;
-
-import java.util.*;
-
-public class P22_MinLightsToActivate {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-    }
-}
