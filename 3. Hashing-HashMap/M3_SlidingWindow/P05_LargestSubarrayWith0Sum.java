ce8677283cd60924595c03d8dff541e7579a03e6
Sanket Harvande <sanketharvande49@gmail.com>
2022-10-23 08:38:48 +0530

    done

---
+++ 3. Hashing-HashMap/M3_SlidingWindow/P05_LargestSubarrayWith0Sum.java
@@ -0,0 +1,9 @@
+package M3_SlidingWindow;
+
+
+
+public class P05_LargestSubarrayWith0Sum {
+    public static void main(String[] args) {
+        
+    }
+}
--- 3. Hashing-HashMap/M3_SlidingWindow/P06_EquilibriumIndex.java
+++ 3. Hashing-HashMap/M3_SlidingWindow/P06_EquilibriumIndex.java
@@ -1,3 +1,43 @@
+package M3_SlidingWindow;
+
+import java.util.*;
+
 public class P06_EquilibriumIndex {
-    
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int ans = findEquilibriumIndex(arr, n);
+        System.out.println(ans);
+
+        sc.close();
+    }
+
+    public static int findEquilibriumIndex(int[] arr, int n) {
+        int sum = 0;
+        int leftsum = 0;
+
+        if (n == 1) {
+            return 0;
+        } else if (n == 2) {
+            return -1;
+        }
+        // find sum of whole array
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            sum -= arr[i];
+            if (leftsum == sum) {
+                return i;
+            }
+            leftsum += arr[i];
+        }
+        return -1;
+    }
 }
